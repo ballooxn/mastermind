c9efaa8ef44c92bba28869bb94db149602891a96
Lincoln <163782652+ballooxn@users.noreply.github.com>
2024-07-17 18:58:11 -0500

    Add frameworks of classes

---
+++ lib/computer.rb
@@ -0,0 +1,6 @@
+class Computer
+  @@color_choices = %w[Red Blue Yellow Green Pink Purple Orange]
+  def choose_starting_code
+    @selected_code = []
+  end
+end
---
+++ lib/game.rb
@@ -0,0 +1,12 @@
+require_relative "display"
+require_relative "computer"
+
+class Game
+  def initialize
+    @board = []
+    @selected_code = []
+  end
+
+  def start_game
+  end
+end
