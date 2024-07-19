e8447b35b7b428cae8db8492d1c009b6bf4b8e07
Lincoln <163782652+ballooxn@users.noreply.github.com>
2024-07-19 12:01:14 -0500

    Add display_all_guesses and display_feedback

--- lib/display.rb
+++ lib/display.rb
@@ -2,4 +2,14 @@ class Display
   def intro
     puts "Welcome to Mastermind!"
   end
+
+  def display_all_guesses(board)
+    board.length.times do |i|
+      puts board[i]
+    end
+  end
+
+  def display_feedback(correct, misplaced)
+    puts "#{correct} correct, #{misplaced} misplaced."
+  end
 end
