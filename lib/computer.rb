af63150695ca03448c771d32d20450cdaa267bae
Lincoln Digby <lincolndigby0@gmail.com>
2024-10-14 17:57:46 -0500

    basic loop and starting functions

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,28 +1,2 @@
 module Computer
-  def self.choose_starting_code(color_choices)
-    selected_code = []
-    4.times do
-      selected_code.push(color_choices.sample)
-    end
-    selected_code
-  end
-
-  def self.choose_guess(board, color_choices, correct_array)
-    guess = []
-    if board.empty?
-      # first guess of the game, no feedback to go over.
-      4.times do
-        guess.push(color_choices.sample)
-      end
-    else
-      4.times do |index|
-        if board[-1][index] == correct_array[index]
-          guess[index] = correct_array[index]
-        else
-          guess.push(color_choices.sample)
-        end
-      end
-    end
-    guess
-  end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,147 +1,76 @@
-require_relative "display"
-require_relative "computer"
-require_relative "player"
-COLOR_CHOICES = %w[red blue yellow green purple orange].freeze
+require_relative "lib/display"
+require_relative "lib/computer"
+require_relative "lib/player"
 
 class Game
   include Computer
   include Display
   include Player
 
+  COLOR_CHOICES = %w[Red Orange Yellow Green Blue Pink Purple].freeze
+  MAX_NUMBER_OF_GUESSES = 12
+
   def initialize
+    @computer_score = 0
+    @human_score = 0
+
+    @guesses = 0
+    @role = nil
+    @winner = nil
+
     @board = []
-    @selected_code = []
-    @guess = []
-    @feedback_array = []
-    @computer_breaking = false
-    @winner = "Creator"
-    @correct_array = []
   end
 
   def start_game
-    Display.intro(COLOR_CHOICES)
-    puts "\nWould you like to play as the Breaker or the Creator? b/c"
-    answer = gets.chomp.downcase
-    if answer == "b"
-      breaker_game_loop
-    elsif answer == "c"
-      creator_game_loop
+    Display.intro
+    choose_role
+    return if role == "exit"
+
+    game_loop
+  end
+
+  def choose_role
+    until role == "coder" || role == "breaker" || role == "exit"
+      Display.choose_role
+      @role = gets.chomp.downcase
+    end
+  end
+
+  def game_loop
+    until @winner || @guesses >= MAX_NUMBER_OF_GUESSES
+      guess = nil
+
+      guess = obtain_guess until valid_guess?(guess)
+
+      place_guess_on_board(guess)
+      # Feedback = {correct: 0, misplaced: 0, incorrect: 0}
+      feedback = evaluate_guess(guess)
+      # end game if the guess was correct
+      @winner = true if feedback[:correct] == 4
+
+      @guesses += 1
+    end
+    end_game
+  end
+
+  def obtain_guess
+    if @role == "breaker"
+      Player.choose_guess
     else
-      puts "ERROR!"
-    end
-  end
-
-  def creator_game_loop
-    puts "Create a code of 4 colors seperated by spaces."
-    create_code
-    game_over = false
-    @computer_breaking = true
-
-    until game_over
-      Display.computer_choosing_guess
-      sleep(1)
-      feedback = []
-      @guess = Computer.choose_guess(@board, COLOR_CHOICES, @correct_array)
-      @board.push(@guess)
-      feedback = make_feedback
-      puts "Computer chose... #{@guess}"
-      Display.display_feedback(feedback[0], feedback[1])
-      sleep(2)
-      Display.display_all_guesses(@board, @feedback_array)
-
-      if feedback[0] == 4 # correct guesses
-        @winner = "Breaker"
-        break
-      end
-      break if @board.length >= 12
-    end
-    end_game
-  end
-
-  def create_code
-    loop do
-      @selected_code = gets.chomp.downcase.split
-      unless @selected_code.length == 4
-        puts "Error! Put in four selectable colors seperated by spaces"
-        next
-      end
-
-      break if valid?(@selected_code)
-
-      puts "Error! Put in four selectable colors seperated by spaces"
-    end
-  end
-
-  def breaker_game_loop
-    @selected_code = Computer.choose_starting_code(COLOR_CHOICES)
-
-    game_over = false
-    until game_over
-      Display.choose_your_guess
-      choose_guess
-      @board.push(@guess)
-      feedback = make_feedback
-      Display.display_feedback(feedback[0], feedback[1])
-      Display.display_all_guesses(@board, @feedback_array)
-
-      if feedback[0] == 4 # correct guesses
-        @winner = "Breaker"
-        break
-      end
-      break if @board.length >= 12
-    end
-    end_game
-  end
-
-  # Checks if the code is valid, returns true if it is.
-  def valid?(array)
-    count = 0
-    array.each do |i|
-      count += 1 if COLOR_CHOICES.include?(i)
-    end
-    return true if count == 4
-
-    false
-  end
-
-  def choose_guess
-    choosing_guess = true
-    while choosing_guess
-      @guess = gets.chomp.downcase.split # this is an array
-      unless @guess.length == 4
-        puts "Error! Put in four selectable colors seperated by spaces"
-        next
-      end
-
-      break if valid?(@guess)
-
-      puts "Error! Put in four colors from this list: #{COLOR_CHOICES}"
-    end
+      Computer.choose_guess end
   end
 
   def end_game
-    Display.end_game_display(@winner)
-    play_again = gets.chomp.downcase
-    Game.new.start_game if play_again == "y"
-  end
-
-  def make_feedback
-    correct = 0
-    misplaced = 0
-    @guess.each_with_index do |color, index|
-      next unless @selected_code.include?(color)
-
-      if @guess[index] == @selected_code[index]
-        @correct_array[index] = color if @computer_breaking
-        correct += 1
-        next
-      end
-      @correct_array[index] = "_" if @computer_breaking
-      misplaced += 1
+    if @guesses >= MAX_NUMBER_OF_GUESSES && !@winner
+      Display.coder_win
+    else
+      Display.breaker_win
     end
-    arr = [correct, misplaced]
-    @feedback_array.push(arr) # This array is used for the display_board method in Display
 
-    arr
+    Display.play_again?
+    answer = gets.chomp.downcase
+    return unless answer == "y" || answer == "yes" # rubocop:disable Style/MultipleComparison
+
+    Game.new.start_game
   end
 end
